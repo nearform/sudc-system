9ef39e86fc408d53e9ccf76ac0b62d02f752295e
Peter Elger <elger.peter@gmail.com>
2014-10-18 20:59:15 +0100

    defined containers

--- definitions/services.js
+++ definitions/services.js
@@ -1,18 +1,53 @@
-// Place service container definitions here.
-
 exports.root = {
   type: 'container'
 };
 
-// Example
-//
-// exports.web = {
-//   type: 'process',
-//     specific: {
-//       repositoryUrl: 'git@github.com:nearform/nscaledemoweb.git',
-//       execute: {
-//         args: '-p 8000:8000 -d',
-//         exec: '/usr/bin/node index.js'
-//       }
-//     }
-// }; 
+exports.doc = {
+  type: 'process',
+  specific: {
+    repositoryUrl: 'git@github.com:nearform/sudc-doc.git',
+    buildScript: 'build.sh',
+    execute: {
+      args: '-p 9002:9002 -d',
+      exec: '/usr/bin/node /srv/doc-srv.js'
+    }
+  }
+};
+
+
+exports.hist = {
+  type: 'process',
+  specific: {
+    repositoryUrl: 'git@github.com:nearform/sudc-history.git',
+    buildScript: 'build.sh',
+    execute: {
+      args: '-p 9000:9000 -d',
+      exec: '/usr/bin/node /srv/hist-srv.js'
+    }
+  }
+};
+
+exports.real = {
+  type: 'process',
+  specific: {
+    repositoryUrl: 'git@github.com:nearform/sudc-realtime.git',
+    buildScript: 'build.sh',
+    execute: {
+      args: '-p 9001:9001 -d',
+      exec: '/usr/bin/node /srv/real-srv.js'
+    }
+  }
+};
+
+exports.web = {
+  type: 'process',
+  specific: {
+    repositoryUrl: 'git@github.com:nearform/sudc-web.git',
+    buildScript: 'build.sh',
+    execute: {
+      args: '-p 8000:8000 -d',
+      exec: '/bin/bash /web/run.sh'
+    }
+  }
+};
+
--- system.js
+++ system.js
@@ -1,19 +1,10 @@
-// Define the system topology here. The topolgy should reference 
-// containers defined in definitions/services.js
-
 exports.name = 'sudc-system';
-exports.namespace = 'sudc-system';
-exports.id = 'f22681a9-4c24-46a5-8bb1-1b64a641ba9b';
+exports.namespace = 'sudc';
+exports.id = '62999e58-66a0-4e50-a870-f2673acf6c79';
 
 exports.topology = {
   local: {
+    root: ['doc', 'hist', 'real', 'web']
   }
 };
 
-// Example
-//
-// exports.topology = {
-//   local: {
-//     root: ['web']
-//   }
-// };
