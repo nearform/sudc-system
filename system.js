29c2525563115ccaea227d3b2438996310af8bff
Peter Elger <elger.peter@gmail.com>
2014-10-18 20:56:40 +0100

    first commit

---
+++ system.js
@@ -0,0 +1,19 @@
+// Define the system topology here. The topolgy should reference 
+// containers defined in definitions/services.js
+
+exports.name = 'sudc-system';
+exports.namespace = 'sudc-system';
+exports.id = 'f22681a9-4c24-46a5-8bb1-1b64a641ba9b';
+
+exports.topology = {
+  local: {
+  }
+};
+
+// Example
+//
+// exports.topology = {
+//   local: {
+//     root: ['web']
+//   }
+// };
