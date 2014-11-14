6a05728734de55da427b7a16a663d86c44e696f3
your name <you@somewhere.com>
2014-11-14 07:44:04 -0500

    system compile

--- system.js
+++ system.js
@@ -5,6 +5,15 @@ exports.id = '62999e58-66a0-4e50-a870-f2673acf6c79';
 exports.topology = {
   local: {
     root: ['doc', 'hist', 'real', 'web']
+  },
+  direct: {
+    machine$123: {
+      contains: ['doc', 'hist', 'real', 'web'],
+      specific: {
+        user: 'root',
+        identityFile: 'sudc-key'
+      }
+    }
   }
 };
 
