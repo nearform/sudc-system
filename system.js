ffc739983e68012df8b93c5ed9b43b940a57add8
Darragh Hayes <dara.hayes.1995@gmail.com>
2015-01-30 12:38:00 +0000

    Using development environment instead of local
    Doesn't work with the workshop using the new version of nscale otherwise

--- system.js
+++ system.js
@@ -3,7 +3,7 @@ exports.namespace = 'sudc';
 exports.id = '62999e58-66a0-4e50-a870-f2673acf6c79';
 
 exports.topology = {
-  local: {
+  development: {
     root: ['doc', 'hist', 'real', 'web']
   }
 };
