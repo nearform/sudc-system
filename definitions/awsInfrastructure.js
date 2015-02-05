ea71fc0faa3b1bbdb6bf13e56ee49f66933547e1
Darragh Hayes <dara.hayes.1995@gmail.com>
2015-02-05 16:20:25 +0000

    added aws definitions for v0.14

---
+++ definitions/awsInfrastructure.js
@@ -0,0 +1,44 @@
+// AWS specific infrastructure
+exports.awsRoot = {
+  type: 'blank-container'
+};
+
+exports.awsWebElb = {
+  type: 'aws-elb',
+  specific: {
+    LoadBalancerName: 'webElb',
+    Listeners: [{Protocol: 'HTTP', LoadBalancerPort: 80, InstanceProtocol: 'HTTP', InstancePort: 8000}],
+    AvailabilityZones: ['us-west-2a'],
+    Subnets: ['subnet-xxxxxxxx']
+  }
+};
+
+exports.awsWebSg = {
+  type: 'aws-sg',
+  specific: {
+    GroupName: 'webSg',
+    Description: 'Web security group',
+    IpPermissions: [
+      { 'IpProtocol': 'tcp', 'FromPort': 3000, 'ToPort': 3000, 'UserIdGroupPairs': [], 'IpRanges': [ { 'CidrIp': '0.0.0.0/0' } ] },
+      { 'IpProtocol': 'tcp', 'FromPort': 22, 'ToPort': 22, 'UserIdGroupPairs': [], 'IpRanges': [ { 'CidrIp': '0.0.0.0/0' } ] },
+      { 'IpProtocol': 'tcp', 'FromPort': 9002, 'ToPort': 9002, 'UserIdGroupPairs': [], 'IpRanges': [ { 'CidrIp': '0.0.0.0/0' } ] },
+      { 'IpProtocol': 'tcp', 'FromPort': 873, 'ToPort': 873, 'UserIdGroupPairs': [], 'IpRanges': [ { 'CidrIp': '10.0.0.0/8' } ] },
+      { 'IpProtocol': 'tcp', 'FromPort': 9001, 'ToPort': 9001, 'UserIdGroupPairs': [], 'IpRanges': [ { 'CidrIp': '0.0.0.0/0' } ] },
+      { 'IpProtocol': 'tcp', 'FromPort': 9000, 'ToPort': 9000, 'UserIdGroupPairs': [], 'IpRanges': [ { 'CidrIp': '0.0.0.0/0' } ] },
+      { 'IpProtocol': 'tcp', 'FromPort': 8000, 'ToPort': 8000, 'UserIdGroupPairs': [], 'IpRanges': [ { 'CidrIp': '0.0.0.0/0' } ] }
+    ],
+    IpPermissionsEgress: [
+      { IpProtocol: '-1', UserIdGroupPairs: [], IpRanges: [ { 'CidrIp': '0.0.0.0/0' } ] } ],
+    VpcId: 'vpc-xxxxxxxx',
+    Tags: []
+  }
+};
+
+exports.awsMachine = {
+  type: 'aws-ami',
+  name: 'demo2',
+  specific: {
+    ImageId: 'ami-xxxxxxxx'
+  }
+};
+
--- definitions/infrastructure.js
+++ definitions/infrastructure.js
@@ -1,3 +1,8 @@
+// infrastructure
+exports.root = {
+  type: 'container'
+};
+
 exports.doc = {
   type: 'process',
   specific: {
