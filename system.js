f911bf0d8324c0a36823f1a9d4e6da5c4f23e517
Matteo Collina <hello@matteocollina.com>
2015-02-24 09:59:43 +0000

    system compile

--- system.js
+++ system.js
@@ -5,8 +5,11 @@ exports.id = '62999e58-66a0-4e50-a870-f2673acf6c79';
 exports.topology = {
   aws: {
     awsWebElb: [{
-      awsWebSg:[{awsMachine: ['web']},
-                {awsMachine: ['doc', 'hist', 'real']}]
+      awsWebSg: [{
+        awsMachine: ['web']
+      }, {
+        awsMachine: ['doc', 'hist', 'real']
+      }]
     }]
   },
   development: {
