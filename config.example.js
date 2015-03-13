fb554d02f261112fa917079009501aa9a594056b
Matteo Collina <matteo.collina@gmail.com>
2015-03-13 13:47:26 +0000

    Local config example.

---
+++ config.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+  region: 'us-west-2',
+  identityFile: "key.pem",
+  accessKeyId: "YOUR_KEY_HERE",
+  secretAccessKey: "YOUR_SECRET_HERE",
+  user: "ubuntu",
+  defaultSubnetId: "subnet-xxxxxxxx",
+  defaultVpcId: "vpc-xxxxxxxx"
+};
